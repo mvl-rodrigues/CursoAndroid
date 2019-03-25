4a2271e4f212dd799b710330077ad7715d7e0221
mvl-rodrigues <victor.ufpaa@gmail.com>
2019-03-25 20:07:19 -0300

    Aula2: criando o formulario

--- app/src/main/java/br/com/alura/agenda2/ListaAlunosActivity.java
+++ app/src/main/java/br/com/alura/agenda2/ListaAlunosActivity.java
@@ -2,6 +2,9 @@ package br.com.alura.agenda2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
@@ -12,5 +15,14 @@ public class ListaAlunosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
         setTitle(TITLE);
+
+        String[] alunos = {"victor","caio","luis","jessica"};
+
+        ListView listaAlunos = findViewById(R.id.activity_lista_alunos_listview);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, alunos);
+
+        listaAlunos.setAdapter(adapter);
+
     }
 }
